e579f5719c51170fe9fbd1e58227c38846ca2c29
AditiRoy171 <royaditi200401@gmail.com>
2025-06-28 16:14:20 +0530

    added eg

--- app.py
+++ app.py
@@ -9,14 +9,34 @@ def classify_pet(img):
     pred, idx, probs = learn.predict(img)
     return f"Prediction: {pred} (Confidence: {probs[idx]:.2f})"
 
+examples = [
+    ["Persian cat.jpeg"],
+    ["Siamese kitten.jpeg"],
+    ["Top 14 Reasons Why Chihuahuas Are Better Than….jpeg"],
+    ["Pug.jpeg"]
+]
+
+description = """
+**Example Images:**
+
+🐱 Persian  &nbsp;&nbsp;&nbsp;&nbsp;
+🐱 Birman &nbsp;&nbsp;&nbsp;&nbsp;
+🐶 Chihuahua &nbsp;&nbsp;&nbsp;&nbsp;
+🐶 Pug
+
+(Click on any image below to try it!)
+"""
+
 # Create Gradio interface
 demo = gr.Interface(
     fn=classify_pet,
     inputs=gr.Image(type="pil"),
     outputs="text",
+    examples=examples,
     title="FastAI Pet Classifier",
     description="Upload a cat or dog image and get the predicted breed."
 )
 
 # Launch app
 demo.launch()
+
